7e47b07501445975e243d5221cde6c8a4b837cd3
salimaelayy <64556840+salimaelayy@users.noreply.github.com>
2024-04-23 18:49:18 +0100

    escaped commas

--- src/component/prefooter/page.tsx
+++ src/component/prefooter/page.tsx
@@ -1,19 +1,20 @@
 import Link from "next/link";
-import { CiInstagram, CiLinkedin, CiTwitter } from "react-icons/ci";
+import { FaInstagram, FaLinkedin, FaTwitter } from "react-icons/fa";
 
 const PreFooter = () => {
   return (
     <>
       <div className="grid grid-cols-1 md:grid-cols-2 gap-8">
         <div className="col-span-1 md:col-span-1">
-          <h2 className="text-7xl font-bold mb-6">Let{'\''}s Work-Together</h2>
+          <h2 className="text-7xl font-bold mb-6">Let's Work-Together</h2>
         </div>
         <div className="col-span-1 md:col-span-1 flex flex-col items-end">
-            <button className="bg-black text-white border border-white py-2 px-4 rounded-lg mb-4 hover:bg-gray-800 hover:border-gray-800 transition duration-300 w-60">Contact Me</button>
-            <button className="bg-white text-black py-2 px-4 rounded-lg hover:bg-gray-200 transition duration-300 w-60">Visit My LinkedIn</button>
+          <Link href="/contact">
+            <a className="bg-black text-white border border-white py-2 px-4 rounded-lg mb-4 hover:bg-gray-800 hover:border-gray-800 transition duration-300 w-60">Contact Me</a>
+          </Link>
+          <a href="https://www.linkedin.com/in/salima-el-ayyachi-fullstack-mern-developer/" target="_blank" className="bg-white text-black py-2 px-4 rounded-lg hover:bg-gray-200 transition duration-300 w-60">Visit My LinkedIn</a>
         </div>
-    </div>
-
+      </div>
     </>
   );
 };
