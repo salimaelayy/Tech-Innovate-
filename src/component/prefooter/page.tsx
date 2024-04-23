924594e81036912950c37a2ccfc7c8c3804fac05
salimaelayy <64556840+salimaelayy@users.noreply.github.com>
2024-04-23 18:34:33 +0100

    escaped comma

--- src/component/prefooter/page.tsx
+++ src/component/prefooter/page.tsx
@@ -6,7 +6,7 @@ const PreFooter = () => {
     <>
       <div className="grid grid-cols-1 md:grid-cols-2 gap-8">
         <div className="col-span-1 md:col-span-1">
-            <h2 className="text-7xl font-bold mb-6">Let's Work-Together</h2>
+            <h2 className="text-7xl font-bold mb-6">Lets Work-Together</h2>
         </div>
         <div className="col-span-1 md:col-span-1 flex flex-col items-end">
             <button className="bg-black text-white border border-white py-2 px-4 rounded-lg mb-4 hover:bg-gray-800 hover:border-gray-800 transition duration-300 w-60">Contact Me</button>
